206ce2a3af5b0ebe634bfb202df546708d6ae7f5
mayksc96 <mayksc96@gmail.com>
2024-03-17 08:57:08 -0500

    Atualizacion curso JS

--- js/1-clase.js
+++ js/1-clase.js
@@ -11,3 +11,28 @@
 */
 // Que son las variables en programacion? 
 // Que tipos de variables existen en JS
+
+//Que tipos de variables existen en JS?
+// Numeros, cadenas (texto ->string), objetos (estructuras mas complejas,
+ // que pueden estar compuestas por mas variables), arreglos, boolean (true or false)...
+
+ 
+ // que carajos es declarar?
+ // es crear una variable
+
+ /*Como podemos declarar variables?
+  se puede hacer por medio de las palabras reservadas ( es que no se deben usar por el
+ programador para guardar informacion) let, var, Const)
+ */
+// para crear una variable se le debe poner un nombre descriptivo 
+//operador asignacion '='
+ // declaramos e iniciamos la variable
+let nombreDeDanny = "danny"; //usamos camelCase
+var apellidoDeCamilo; // simplemente se declaro
+
+// hay mundo de codigo
+
+ apellidoDeCamilo = "Alvarez";  //se le asigna un valor a la variable apellidoDECamilo
+
+
+const salarioMinimo2024 = 1300000; // definimos una constante */
